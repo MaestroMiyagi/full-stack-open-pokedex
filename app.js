ef4b9a970195ccf3023681b5b21b7ffe41427b19
MaestroMiyagi <mffchmono@gmail.com>
2024-12-23 17:13:14 -0600

    health

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const app = express();
 
-// get the port from env variable
 const PORT = process.env.PORT || 5000;
+
+app.get("/health", (req, res) => {
+  res.send("ok");
+});
+
+app.get("/version", (req, res) => {
+  res.send("8");
+});
 
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
-
-app.get('/version', (req, res) => {
-  res.send('8') // change this string to ensure a new version deployed
-})
-
-app.get('/health', (req, res) => {
-  res.send('ok')
-})
